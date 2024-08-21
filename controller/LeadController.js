6d08aae4e98f4c91c6504147145b96d433ba26ff
Settu1998 <settubsc98@gmail.com>
2024-08-22 00:44:02 +0530

    backend

--- controller/LeadController.js
+++ controller/LeadController.js
@@ -18,6 +18,57 @@ async function getLead(req,res) {
     }
 }
 
+
+async function UpateLead(req,res) {
+    try {
+
+        const { id,email,mobile,product,name } = req.body
+           console.log(req.body);
+           
+        await db('team_lead').update({
+            name:name,
+            email:email,
+            number:mobile,
+            product:product
+        }).where({id:id});
+        return res.send(JSON.stringify({
+            code:200,
+            status:"success",
+            message:"Updated team lead successfully"
+        }))
+
+    } catch (error) {
+        return res.send(JSON.stringify({
+            code:500,
+            status:"error",
+            message:error.message
+        })) 
+    }
+}
+
+async function deleteLead(req,res) {
+    try {
+
+        const { id } = req.body
+
+        await db('team_lead').delete().where({id:id});
+        return res.send(JSON.stringify({
+            code:200,
+            status:"success",
+            message:"Delete lead successfully"
+        }))
+
+    } catch (error) {
+        return res.send(JSON.stringify({
+            code:500,
+            status:"error",
+            message:error.message
+        })) 
+    }
+}
+
 module.exports = {
-    getLead
+    getLead,
+    UpateLead,
+    deleteLead
 }
--- routes/lead.js
+++ routes/lead.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const { 
-    getLead 
+    getLead,
+    UpateLead,
+    deleteLead 
  } = require('../controller/LeadController')
 const route = express.Router();
 
 
 
 route.get('/getlead',getLead)
+route.patch('/UpateLead',UpateLead)
+route.post('/deleteLead',deleteLead)
 module.exports  = route;
